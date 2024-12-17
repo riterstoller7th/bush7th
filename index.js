fa145c992fc1c83486973b565d3e9eeadb56f821
riterstoller7th <484711@bsd48.org>
2024-12-17 11:40:33 -0800

    tech class done, got second stanza done

---
+++ index.js
@@ -0,0 +1,12 @@
+x = 0
+function() {
+  x = x + 1
+}
+if(x==0)
+  document.getElementbyID("story").innerHTML = "I'm in the thick of it, everybody knows"
+if(x==1)
+  document.getElementbyID("story").innerHTML = "They know me where it snows, I skied in and they froze, I don't know no nothin' 'bout no ice, I'm just cold"
+if(x==2)
+  document.getElementbyID("story").innerHTML = "Forty somethin' milli' subs or so, I've been told"
+if(x==3)
+  document.getElementbyID("I'm in my prime and this ain't even final form, They knocked me down, but still, my feet, they find the floor, I went from living rooms straight out to sold-out tours, Life's a fight, but trust, I'm ready for the war")
