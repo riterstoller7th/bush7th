206d710131995dd59b0a4fd2f367e030d1c3e173
riterstoller7th <484711@bsd48.org>
2024-12-17 23:32:31 -0800

    finished debugging, hopefully it works

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
-x = 0
-function() {
+x = -1
+function thickofit() {
   x = x + 1
 if(x==0)
-  document.getElementbyID("story").innerHTML = "I\'m in the thick of it, everybody knows"
+  document.getElementbyId("story").innerHTML = "I\'m in the thick of it, everybody knows"
 else if(x==1)
-  document.getElementbyID("story").innerHTML = "They know me where it snows, I skied in and they froze, I don\'t know no nothin\' \'bout no ice, I\'m just cold"
+  document.getElementById("story").innerHTML = "They know me where it snows, I skied in and they froze, I don\'t know no nothin\' \'bout no ice, I\'m just cold"
 else if(x==2)
-  document.getElementbyID("story").innerHTML = "Forty somethin\' milli\' subs or so, I\'ve been told"
+  document.getElementById("story").innerHTML = "Forty somethin\' milli\' subs or so, I\'ve been told"
 else if(x==3)
-  document.getElementbyID("story").innerHTML = "I\'m in my prime and this ain\'t even final form. They knocked me down, but still, my feet, they find the floor"
+  document.getElementById("story").innerHTML = "I\'m in my prime and this ain\'t even final form. They knocked me down, but still, my feet, they find the floor"
 else if(x==4)
-  document.getElementbyID("story").innerHTML = "I went from living rooms straight out to sold-out tours. Life\'s a fight, but trust, I\'m ready for the war"
+  document.getElementById("story").innerHTML = "I went from living rooms straight out to sold-out tours. Life\'s a fight, but trust, I\'m ready for the war"
 else if(x==5)
-  document.getElementbyID("story").innerHTML = "Woah-oh-oh, This is how the story goes. Woah-oh-oh, I guess this is how the story goes."
+  document.getElementById("story").innerHTML = "Woah-oh-oh, This is how the story goes. Woah-oh-oh, I guess this is how the story goes."
 else if(x==6)
-  document.getElementbyID("story").innerHTML = "I\'m in the thick of it, everybody knows. They know me where it snows, I skied in and they froze"
+  document.getElementById("story").innerHTML = "I\'m in the thick of it, everybody knows. They know me where it snows, I skied in and they froze"
 else if(x==7)
-  document.getElementbyID("story").innerHTML = "I don\'t know no nothin\' \'bout no ice, I\'m just cold. Forty somethin\' milli\' subs or so, I\'ve been told"
+  document.getElementById("story").innerHTML = "I don\'t know no nothin\' \'bout no ice, I\'m just cold. Forty somethin\' milli\' subs or so, I\'ve been told"
 else if(x==8)
-  document.getElementbyID("story").innerHTML = "From the screen to the ring, to the pen, to the king. Where\'s my crown? That\'s my bling, always drama when I ring"
+  document.getElementById("story").innerHTML = "From the screen to the ring, to the pen, to the king. Where\'s my crown? That\'s my bling, always drama when I ring"
 else if(x==9)
-  document.getElementbyID("story").innerHTML = "See, I believe that if I see it in my heart. Smash through the ceiling \'cause I\'m reachin\' for the stars"
+  document.getElementById("story").innerHTML = "See, I believe that if I see it in my heart. Smash through the ceiling \'cause I\'m reachin\' for the stars"
 else if(x==10)
-  document.getElementbyID("story").innerHTML = "Woah-oh-oh, This is how the story goes. Woah-oh-oh, I guess this is how the story goes"
+  document.getElementById("story").innerHTML = "Woah-oh-oh, This is how the story goes. Woah-oh-oh, I guess this is how the story goes"
 else if(x==11)
-  document.getElementbyID("story").innerHTML = "I\'m in the thick of it, everybody knows. They know me where it snows, I skied in and they froze"
+  document.getElementById("story").innerHTML = "I\'m in the thick of it, everybody knows. They know me where it snows, I skied in and they froze"
 else if(x==12)
-  document.getElementbyID("story").innerHTML = "I don\'t know no nothin\' \'bout no ice, I\'m just cold. Forty somethin\' milli\' subs or so, I\'ve been told"
+  document.getElementById("story").innerHTML = "I don\'t know no nothin\' \'bout no ice, I\'m just cold. Forty somethin\' milli\' subs or so, I\'ve been told"
 else if(x==13)
-  document.getElementbyID("story").innerHTML = "Highway to heaven, I\'m just cruisin\' by my lone.\' They cast me out, left me for dead, them people cold. My faith in God, mind in the sun, I\'m \'bout to sow"
+  document.getElementById("story").innerHTML = "Highway to heaven, I\'m just cruisin\' by my lone.\' They cast me out, left me for dead, them people cold. My faith in God, mind in the sun, I\'m \'bout to sow"
 else if(x==14)
-  document.getElementbyID("story").innerHTML = "My life is hard, I took the wheel, I cracked the code. Ain\'t nobody gon\' save you, man, this life will break you. In the thick of it, this is how the story goes"
+  document.getElementById("story").innerHTML = "My life is hard, I took the wheel, I cracked the code. Ain\'t nobody gon\' save you, man, this life will break you. In the thick of it, this is how the story goes"
 else if(x==15)
-  document.getElementbyID("story").innerHTML = "I\'m in the thick of it, everybody knows. They know me where it snows, I skied in and they froze. I don\'t know no nothin\' \'bout no ice, I\'m just cold. Forty somethin\' milli\' subs or so, I\'ve been told x 2"
+  document.getElementById("story").innerHTML = "I\'m in the thick of it, everybody knows. They know me where it snows, I skied in and they froze. I don\'t know no nothin\' \'bout no ice, I\'m just cold. Forty somethin\' milli\' subs or so, I\'ve been told x 2"
 else if(x==16)
-  document.getElementbyID("story").innerHTML = "Woah-oh-oh (nah-nah-nah-nah, ayy, ayy). This is how the story goes (nah, nah) Woah-oh-oh, I guess this is how the story goes!"
+  document.getElementById("story").innerHTML = "Woah-oh-oh (nah-nah-nah-nah, ayy, ayy). This is how the story goes (nah, nah) Woah-oh-oh, I guess this is how the story goes!"
 }
